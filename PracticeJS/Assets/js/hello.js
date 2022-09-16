e1d4c28e8a47f08f0606b1cabb58fe12a5fbf5f7
AugustNewby <august.newby@students.first.edu>
2022-09-15 21:47:28 -0500

    Yes Sir

---
+++ PracticeJS/Assets/js/hello.js
@@ -0,0 +1,38 @@
+
+ // DECLARE FUNCTION
+
+// function doSomething () {
+//     console.log("HELLO WORLD");
+// }
+
+// doSomething();
+
+// function square(x) {
+//     return x * x;
+// }
+
+function sayHello() {
+    console.log("Hi!"); 
+}
+
+
+function plusFive(num) {
+    return num + 5; 
+}
+    
+function increaseLargerNumberByFive(num1, num2) {
+    var larger;
+
+    if (num1 >= num2) { 
+    larger = num1; 
+} 
+    else { 
+    larger = num2; 
+}
+    return plusFive(larger); 
+} 
+
+// // Function Execution code
+// sayHello();
+// console.log(increaseLargerNumberByFive(4,3));
+    
