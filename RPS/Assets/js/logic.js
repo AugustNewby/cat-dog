911c479b4adb882da4bb494927abaeb4631ce939
AugustNewby <august.newby@students.first.edu>
2022-09-20 21:50:43 -0500

    nn

--- RPS/Assets/js/logic.js
+++ RPS/Assets/js/logic.js
@@ -46,7 +46,7 @@ function playGame (yourPick) {
    
     let myPick = getRandomPick();
     
-    let rockleft = document.getElementById("rockLeft");
+    let rockLeft = document.getElementById("rockLeft");
     let paperLeft = document.getElementById ("paperLeft");
     let scissorsLeft = document.getElementById ("scissorsLeft");
     let rockRight = document.getElementById ("rockRight");
@@ -58,7 +58,7 @@ function playGame (yourPick) {
     yourPickText.innerHTML = yourPick;
     switch (yourPick) {
     case ROCK:
-    rockleft.classlist.add("selected");
+    rockLeft.classList.add("selected");
     break;
     case PAPER:
     paperLeft.classList.add("selected");
@@ -84,4 +84,57 @@ switch (myPick) {
 let result = document - getElementById("result");
 
 result.innerHTML = getResultText (myPick, yourPick);
+}
+
+function setUp() {
+    let game = document.getElementById("game");
+    game.onanimationend = function() {
+        this.classList.remove("animate");
+    }
+
+    let closer = document.querySelectorAll(".closer");
+    for (let i = 0; i < closer.length; i++) {
+        let closer = closer[i];
+        closer.onclick = function() {
+
+            var popupContainer = document.getElementById("popUpContainer");
+            popupContainer.classList.remove("popped");
+            resetGame();
+        };
+    }
+}
+
+function resetGame() {
+    let rockLeft = document.getElementById("rockLeft");
+    let paperLeft = document.getElementById("paperLeft");
+    let scissorsLeft = document.getElementById("scissorsLeft");
+    let rockRight = document.getElementById("rockRight");
+    let paperRight = document.getElementById("paperRight");
+    let scissorsRight = document.getElementById("scissorsRight");
+    let yourPickText = document.getElementById("yourPickText");
+    let myPickText = document.getElementById("myPickText");
+
+    yourPickText.innerHTML = "";
+    myPickText.innerHTML = "";
+
+    rockLeft.classList.remove("selected");
+    paperLeft.classList.remove("selected");
+    scissorsLeft.classList.remove("selected");
+    rockRight.classList.remove("selected");
+    paperRight.classList.remove("selected");
+    scissorsRight.classList.remove("selected");
+
+    let result = document.getElementById("result");
+    result.innerHTML = "";
+}
+
+function popAndPlay(yourPick) {
+    
+    let popupContainer = document.getElementById("popupContainer");
+    let game = document.getElementById("game");
+
+    popupContainer.classList.add("popped");
+    game.classList.add("animate");
+
+    playGame(yourPick);
 }
