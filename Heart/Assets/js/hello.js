b7362f2d17cb597da2d8b5589daf57b3af987445
AugustNewby <august.newby@students.first.edu>
2022-09-22 20:45:50 -0500

    Yes

--- Heart/Assets/js/hello.js
+++ Heart/Assets/js/hello.js
@@ -1,14 +1,14 @@
 let starter = document.getElementById("starter");
 
 let imgArray = new Array
-('assets/Images/Asset20.png',
- 'assets/Images/Asset21.png',
- 'assets/Images/Asset22.png',
- 'assets/Images/Asset23.png',
- 'assets/Images/Asset24.png',
- 'assets/Images/Asset25.png',
- 'assets/Images/Asset26.png',
- 'assets/Images/Asset27.png');
+('Assets/Images/Asset20.png',
+ 'Assets/Images/Asset21.png',
+ 'Assets/Images/Asset22.png',
+ 'Assets/Images/Asset23.png',
+ 'Assets/Images/Asset24.png',
+ 'Assets/Images/Asset25.png',
+ 'Assets/Images/Asset26.png',
+ 'Assets/Images/Asset27.png');
 let count = 0;
 
 function animate() {
